1fe2905cecee73ead4ccc6b0e691268b842b8845
baloxegal <baloxegal@gmail.com>
2021-03-31 17:05:17 +0300

    +/+/+/+/

--- ACDB/Script3.sql
+++ ACDB/Script3.sql
@@ -2,15 +2,35 @@
 
 /*1*/
 select Last_Name from customers order by Last_Name
+
+select distinct Last_Name from customers order by Last_Name
+
+select Last_Name from customers group by customers.Last_Name order by Last_Name
+
+select Last_Name from customers group by customers.Last_Name order by LEFT(Last_Name, 1), len(Last_Name)
+
 /*2*/
 select avg(monthly_payment) as avg_monthly_payment from packages
+
+select avg(packages.monthly_payment - customers.monthly_discount) as customers_payments from customers join packages on customers.pack_id = packages.pack_id
+
 /*3*/
 select Last_Name from customers order by Last_Name desc
+
+select distinct Last_Name from customers order by Last_Name desc
+
+select Last_Name from customers group by customers.Last_Name order by Last_Name desc
+
+select Last_Name from customers group by customers.Last_Name order by LEFT(Last_Name, 1) desc, len(Last_Name) desc
+
 /*4*/
 select count(*) as package_number from packages
+
 /*5*/
 select count(*) as customers_number from customers
+
 /*6*/
-select count(distinct Last_Name) as distinct_last_name_states from customers
+select count(distinct [state]) as distinct_states from customers
+
 /*7*/
 select count(distinct speed) as package_number from packages
