6d27b52c71c4846eb3115814946a3061607a6b88
baloxegal <baloxegal@gmail.com>
2021-04-01 12:09:33 +0300

    final 2.3

--- ACDB/Script3.sql
+++ ACDB/Script3.sql
@@ -64,5 +64,61 @@ select sector_id, max(monthly_payment) as max_monthly_payment from packages grou
 select sector_id, count(sector_id) as number_of_packs_in_sector, max(monthly_payment) as max_monthly_payment from packages group by sector_id
 
 /*5*/
-select pack_id, avg(monthly_discount) as customers_number from customers group by pack_id
-select count(pack_id), avg(monthly_discount) as customers_number from customers group by pack_id
+select count(pack_id) as number_pack_id, avg(monthly_discount) avg_monthly_discount from customers
+
+/*6*/
+select pack_id, avg(monthly_discount) as avg_monthly_discount from customers group by pack_id /*having pack_id is not null*/
+
+select pack_id, count(*) as number_of_consumers_by_pack, avg(monthly_discount) as avg_monthly_discount from customers group by pack_id /*order by pack_id*/
+
+/*7*/
+select pack_id, avg(monthly_discount) as avg_monthly_discount from customers group by pack_id having pack_id in(22, 13)
+
+select pack_id, count(*) as number_of_consumers_by_pack, avg(monthly_discount) as avg_monthly_discount from customers group by pack_id having pack_id in(22, 13)
+
+/*8*/
+select speed, max(monthly_payment) as max_monthly_payment, min(monthly_payment) as min_monthly_payment, avg(monthly_payment) as avg_monthly_payment
+from packages group by speed
+
+/*9*/
+select pack_id, count(*) as number_of_customers from customers group by pack_id having pack_id is not null
+
+select pack_id, count(*) as number_of_customers from customers group by pack_id
+
+/*10*/
+
+/*The same as the 9*/
+
+/*11*/
+select pack_id, count(*) as number_of_customers from customers where monthly_discount > 20 group by pack_id having pack_id is not null
+
+select pack_id, count(*) as number_of_customers from customers where monthly_discount > 20 group by pack_id
+
+/*12*/
+select pack_id, count(*) as number_of_customers from customers group by pack_id having pack_id is not null and count(*) > 100
+
+select pack_id, count(*) as number_of_customers from customers group by pack_id having count(*) > 100
+
+/*13*/
+select state, city, count(*) as customer_number from customers group by state, city order by state, city
+
+/*14*/
+select count(distinct city) as sity_number, avg(monthly_discount) as avg_monthly_discount from customers
+
+/*15*/
+select city, avg(monthly_discount) as avg_monthly_discount from customers group by city
+
+/*16*/
+select city, avg(monthly_discount) as avg_monthly_discount from customers where monthly_discount > 20 group by city
+
+/*17*/
+select count(distinct state), min(monthly_discount) as min_monthly_discount from customers
+
+/*18*/
+select state, min(monthly_discount) as min_monthly_discount from customers group by state
+
+/*19*/
+select state, min(monthly_discount) as min_monthly_discount from customers group by state having min(monthly_discount) > 10
+
+/*20*/
+select speed, count(pack_id) as number_of_packages from packages group by speed having count(pack_id) > 8
